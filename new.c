cd39fe47c584d956ec7af6ba000ba95f499d5bda
mbbbackus-windows <mbbbackus@gmail.com>
2015-09-21 13:12:40 -0400

    Fixed launcher direction, formatting

--- new.c
+++ new.c
@@ -12,68 +12,70 @@
 
 task main()
 {
-//Create "deadzone" variables. Adjust threshold value to increase/decrease deadzone
-int X2 = 0, Y1 = 0, X1 = 0, threshold = 15;
+	//Create "deadzone" variables. Adjust threshold value to increase/decrease deadzone
+	int X2 = 0, Y1 = 0, X1 = 0, threshold = 15;
 
-//Loop Forever
-while(1 == 1)
-{
-//Create "deadzone" for Y1/Ch3
-if(abs(vexRT[Ch3]) > threshold)
-Y1 = vexRT[Ch3];
-else
-Y1 = 0;
-//Create "deadzone" for X1/Ch4
-if(abs(vexRT[Ch4]) > threshold)
-X1 = vexRT[Ch4];
-else
-X1 = 0;
-//Create "deadzone" for X2/Ch1
-if(abs(vexRT[Ch1]) > threshold)
-X2 = vexRT[Ch1];
-else
-X2 = 0;
+	//Loop Forever
+	while(1 == 1)
+	{
+		//Create "deadzone" for Y1/Ch3
+		if(abs(vexRT[Ch3]) > threshold)
+			Y1 = vexRT[Ch3];
+		else
+			Y1 = 0;
+		//Create "deadzone" for X1/Ch4
+		if(abs(vexRT[Ch4]) > threshold)
+			X1 = vexRT[Ch4];
+		else
+			X1 = 0;
+		//Create "deadzone" for X2/Ch1
+		if(abs(vexRT[Ch1]) > threshold)
+			X2 = vexRT[Ch1];
+		else
+			X2 = 0;
 
-//Remote Control Commands
-motor[FrontRightDrive] = Y1 - X2 - X1;
-motor[BackRightDrive] =  Y1 - X2 + X1;
-motor[FrontLeftDrive] = Y1 + X2 + X1;
-motor[BackLeftDrive] =  Y1 + X2 - X1;
-//Intake
-if(vexRT[Btn6U] == 0){
-motor[Intake] = 0;
-}
-if(vexRT[Btn6U] == 1){
-motor[Intake] = -100;
-}
-//Outtake
-if(vexRT[Btn6UXmtr2] == 0){
-motor[Outtake] = 0;
-}
-if(vexRT[Btn6UXmtr2] == 1){
-motor[Outtake] = -100;
-}
-//Launcher
-if(vexRT[Btn7UXmtr2] == 0){
-motor[LauncherLeft] = 0;
-motor[LauncherRight] = 0;
-}
-if(vexRT[Btn7UXmtr2] == 1){
-motor[LauncherLeft] = 100;
-motor[LauncherRight] = 100;
-}
-//Turntable
-if(vexRT[Btn5UXmtr2] == 0 && vexRT[Btn5DXmtr2] == 0){
-motor[TurntableLeft] = 0;
-motor[TurntableRight] = 0;
-}
-if(vexRT[Btn5UXmtr2] == 1){
-motor[TurntableLeft] = 100;
-motor[TurntableRight] = 100;
-}
-if(vexRT[Btn5DXmtr2] == 1){
-motor[TurntableLeft] = -100;
-motor[TurntableRight] = -100;
-}
-}
+		//Remote Control Commands
+		motor[FrontRightDrive] = Y1 - X2 - X1;
+		motor[BackRightDrive] =  Y1 - X2 + X1;
+		motor[FrontLeftDrive] = Y1 + X2 + X1;
+		motor[BackLeftDrive] =  Y1 + X2 - X1;
+
+		//Intake
+		if(vexRT[Btn6UXmtr2] == 0 && vexRT[Btn6DXmtr2] == 0){
+			motor[Outtake] = 0;
+			motor[Intake] = 0;
+		}
+		if(vexRT[Btn6UXmtr2] == 1){
+			motor[Outtake] = -100;
+			motor[Intake] = -100;
+		}
+		if(vexRT[Btn6DXmtr2] == 1){
+			motor[Outtake] = 100;
+			motor[Intake] = 100;
+		}
+
+		//Launcher
+		if(vexRT[Btn7UXmtr2] == 0){
+			motor[LauncherLeft] = 0;
+			motor[LauncherRight] = 0;
+		}
+		if(vexRT[Btn7UXmtr2] == 1){
+			motor[LauncherLeft] = -100;
+			motor[LauncherRight] = -100;
+		}
+
+		//Turntable
+		if(vexRT[Btn5UXmtr2] == 0 && vexRT[Btn5DXmtr2] == 0){
+			motor[TurntableLeft] = 0;
+			motor[TurntableRight] = 0;
+		}
+		if(vexRT[Btn5UXmtr2] == 1){
+			motor[TurntableLeft] = 100;
+			motor[TurntableRight] = 100;
+		}
+		if(vexRT[Btn5DXmtr2] == 1){
+			motor[TurntableLeft] = -100;
+			motor[TurntableRight] = -100;
+		}
+	}
 }
