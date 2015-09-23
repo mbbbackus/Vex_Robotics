134cbe3346699c488a362eca106d2d9b2448249a
John <thesupplier8888@gmail.com>
2015-09-23 22:55:09 +1200

    Added deadzone func
    since deadzone is theoretically reusable, you should probably make it a function :)

--- motor_functions.c
+++ motor_functions.c
@@ -1,23 +1,29 @@
 int X2 = 0, Y1 = 0, X1 = 0, threshold = 15;
 
+//can move to other file, or remove
+void deadZoneCheck();
+{
+	//Create "deadzone" for Y1/Ch3
+	if(abs(vexRT[Ch3]) > threshold)
+		Y1 = vexRT[Ch3];
+	else
+		Y1 = 0;
+	//Create "deadzone" for X1/Ch4
+	if(abs(vexRT[Ch4]) > threshold)
+		X1 = vexRT[Ch4];
+	else
+		X1 = 0;
+	//Create "deadzone" for X2/Ch1
+	if(abs(vexRT[Ch1]) > threshold)
+		X2 = vexRT[Ch1];
+	else
+		X2 = 0;
+}
+
 //Loop Forever
 void mdrive()
 {
-		//Create "deadzone" for Y1/Ch3
-		if(abs(vexRT[Ch3]) > threshold)
-			Y1 = vexRT[Ch3];
-		else
-			Y1 = 0;
-		//Create "deadzone" for X1/Ch4
-		if(abs(vexRT[Ch4]) > threshold)
-			X1 = vexRT[Ch4];
-		else
-			X1 = 0;
-		//Create "deadzone" for X2/Ch1
-		if(abs(vexRT[Ch1]) > threshold)
-			X2 = vexRT[Ch1];
-		else
-			X2 = 0;
+		deadZoneCheck();
 
 		//Remote Control Commands
 		motor[drive_right_front] = Y1 - X2 - X1;
