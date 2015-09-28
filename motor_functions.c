5bdd25407c4e579c19d58528288e07106c8ba8f8
mbbbackus-windows <mbbbackus@gmail.com>
2015-09-28 17:05:43 -0400

    Updated functions, lcd, field-centric control

--- motor_functions.c
+++ motor_functions.c
@@ -1,3 +1,4 @@
+//#include "new.c"
 int X2 = 0, Y1 = 0, X1 = 0, threshold = 15;
 
 //can move to other file, or remove
@@ -26,13 +27,13 @@ void mdrive()
 		deadZoneCheck();
 
 		//Remote Control Commands
-		motor[drive_right_front] = Y1 - X2 - X1;
-		motor[drive_right_back] =  Y1 - X2 + X1;
-		motor[drive_left_front] = Y1 + X2 + X1;
-		motor[drive_left_back] =  Y1 + X2 - X1;
+		motor[FrontRightDrive] = Y1 - X2 - X1;
+		motor[BackRightDrive] =  Y1 - X2 + X1;
+		motor[FrontLeftDrive] = Y1 + X2 + X1;
+		motor[BackLeftDrive] =  Y1 + X2 - X1;
 }
 
-//Loop this :)
+//Loop this
 void intake() // 1 forward, 0 backward
 {
 
@@ -52,4 +53,48 @@ void intake() // 1 forward, 0 backward
 	motor[Outtake] = 0;
 	motor[Intake] = 0;
 	}
+}
+
+//Loop this too
+void launcher()
+{
+		if(vexRT[Btn7UXmtr2] == 0 && vexRT[Btn7RXmtr2] == 0 && vexRT[Btn7DXmtr2] == 0 && vexRT[Btn7LXmtr2] == 0){
+			motor[LauncherLeft] = 0;
+			motor[LauncherRight] = 0;
+		}
+		if(vexRT[Btn7UXmtr2] == 1 && vexRT[Btn7RXmtr2] == 0 && vexRT[Btn7DXmtr2] == 0 && vexRT[Btn7LXmtr2] == 0){
+			motor[LauncherLeft] = -90;
+			motor[LauncherRight] = -90;
+		}
+		if(vexRT[Btn7UXmtr2] == 0 && vexRT[Btn7RXmtr2] == 1 && vexRT[Btn7DXmtr2] == 0 && vexRT[Btn7LXmtr2] == 0){
+			motor[LauncherLeft] = -80;
+			motor[LauncherRight] = -80;
+		}
+		if(vexRT[Btn7UXmtr2] == 0 && vexRT[Btn7RXmtr2] == 0 && vexRT[Btn7DXmtr2] == 1 && vexRT[Btn7LXmtr2] == 0){
+			motor[LauncherLeft] = -70;
+			motor[LauncherRight] = -70;
+		}
+		if(vexRT[Btn7UXmtr2] == 0 && vexRT[Btn7RXmtr2] == 0 && vexRT[Btn7DXmtr2] == 0 && vexRT[Btn7LXmtr2] == 1){
+			motor[LauncherLeft] = -60;
+			motor[LauncherRight] = -60;
+		}
+
+}
+
+//Turntable
+void turntable()
+{
+
+		if(vexRT[Btn5UXmtr2] == 0 && vexRT[Btn5DXmtr2] == 0){
+			motor[TurntableLeft] = 0;
+			motor[TurntableRight] = 0;
+		}
+		if(vexRT[Btn5UXmtr2] == 1){
+			motor[TurntableLeft] = 100;
+			motor[TurntableRight] = 100;
+		}
+		if(vexRT[Btn5DXmtr2] == 1){
+			motor[TurntableLeft] = -100;
+			motor[TurntableRight] = -100;
+		}
 }
