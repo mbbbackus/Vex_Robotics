890073e9ac6eb0a014e7ddc72c578054411ecded
brockly <brockly2@gmail.com>
2015-10-18 11:43:51 -0400

    Updated autonomous

--- autonomous.c
+++ autonomous.c
@@ -1,12 +1,13 @@
 void runAuton()
 {
 	driveStraight(50);
+	launch(70);
 	wait1Msec(1000);
 	driveStraight(0);
-	launch(80);
-	wait1Msec(4000);
-	grab(80);
-	wait1Msec(8000);
+	wait1Msec(2000);
+	grab(100);
+	wait1Msec(10000);
 	grab(0);
 	launch(0);
+	wait1Msec(1000);
 }
