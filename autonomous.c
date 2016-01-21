0ab73e3660ad4b98ce828b5f41c22a7374cab533
mbbbackus-windows <mbbbackus@gmail.com>
2016-01-21 15:36:39 -0500

    Recent Post Competition Commit

--- autonomous.c
+++ autonomous.c
@@ -1,7 +1,47 @@
 //launches preloaded balls from the starting square
-void runAuton()
+void launch1(){
+		for(int low = 0; low < 2; low++){
+			grab(80);
+			wait1Msec(900);
+			grab(0);
+			wait1Msec(1600);
+			drive(70,70,70,70);
+			wait1Msec(400);
+			drive(0,0,0,0);
+		}
+	}
+void launch2Blu(){
+		for(int low = 0; low < 3; low++){
+			turnLauncher(80);
+			wait1Msec(100);
+			turnLauncher(0);
+			grab(80);
+			wait1Msec(900);
+			grab(0);
+			wait1Msec(1600);
+			drive(70,70,70,70);
+			wait1Msec(400);
+			drive(0,0,0,0);
+		}
+}
+void launch2Red(){
+	for(int low = 0; low < 3; low++){
+			turnLauncher(-80);
+			wait1Msec(100);
+			turnLauncher(0);
+			grab(80);
+			wait1Msec(900);
+			grab(0);
+			wait1Msec(1600);
+			drive(70,70,70,70);
+			wait1Msec(400);
+			drive(0,0,0,0);
+		}
+}
+void runAutonBlu()
 {
-	launch(57);
+	//old autonomous
+	/*launch(80);
 	//normally 75
 	wait1Msec(5000);
 
@@ -15,45 +55,63 @@ void runAuton()
 	wait1Msec(1500);
 	grab(0);
 	launch(0);
+	*/
 
+	drive(80,80,80,80);
+	wait1Msec(1000);
+	drive(80,-80,80,-80);
+	wait1Msec(450);
+	drive(0,0,0,0);
+	turnLauncher(80);
+	wait1Msec(1600);
+	turnLauncher(0);
+	launch(65);
+	wait1Msec(5000);
+	drive(70,70,70,70);
+	wait1Msec(300);
+	drive(0,0,0,0);
+	launch1();
+	//drive(70,70,70,70);
+	wait1Msec(500);
+	//drive(0,0,0,0)
+	launch2Blu();
+	wait1Msec(500);
+	//drive(70,70,70,70);
+	launch2Blu();
+	wait1Msec(500);
+	drive(0,0,0,0);
 }
+
+void runAutonRed(){
+
+	drive(80,80,80,80);
+	wait1Msec(1000);
+	drive(-80,80,-80,80);
+	wait1Msec(450);
+	drive(0,0,0,0);
+	turnLauncher(-80);
+	wait1Msec(1600);
+	turnLauncher(0);
+	launch(65);
+	wait1Msec(5000);
+	drive(70,70,70,70);
+	wait1Msec(300);
+	drive(0,0,0,0);
+	launch1();
+	//drive(70,70,70,70);
+	wait1Msec(500);
+	//drive(0,0,0,0)
+	launch2Red();
+	wait1Msec(500);
+	//drive(70,70,70,70);
+	launch2Red();
+	wait1Msec(500);
+	drive(0,0,0,0);
+}
+
 void runProgrammingSkills()
 {
-	launch(48);
+	launch(70);
 	grab(80);
-	//normally 75
 
-	/*wait1Msec(5000);
-
-	for(int low = 0; low < 3; low++){
-		grab(80);
-		wait1Msec(900);
-		grab(0);
-		wait1Msec(1600);
-	}
-	grab(80);
-	wait1Msec(1500);
-	grab(0);
-	launch(0);
-	drive(100, 100, 100, 100);
-	wait1Msec(700);
-	drive(100, -60, 100, -60);
-	grab(80);
-	wait1Msec(700);
-	drive(0,0,0,0);
-	turnLauncher(100);
-	wait1Msec(500);
-	turnLauncher(0);
-	launch(46);
-	wait1Msec(5000);
-	for(int low = 0; low < 3; low++){
-		grab(80);
-		wait1Msec(900);
-		grab(0);
-		wait1Msec(1600);
-	}
-	grab(80);
-	wait1Msec(1500);
-	grab(0);
-	launch(0);*/
 }
--- motor_functions.c
+++ motor_functions.c
@@ -28,93 +28,6 @@ void checkintake() // 1 forward, 0 backward
 	}
 }
 
-//function that controls the power of the launcher
-//loops continuosly as the button is pressed
-task launcher()
-{
-	// Highest -------> Lowest
-	// 7U 7R 7D 7L 8U 8R 8D 8L
-	while(1==1){
-		//None pressed
-		if(vexRT[Btn7LXmtr2] == 0 && vexRT[Btn7UXmtr2] == 0 && vexRT[Btn7RXmtr2] == 0 && vexRT[Btn7DXmtr2] == 0){
-			motor[LauncherLeftOutside] =  0;
-			motor[LauncherRightOutside] = 0;
-			motor[LauncherLeftInside] =   0;
-			motor[LauncherRightInside] =  0;
-		}
-		//7L -- Lowest
-		if(vexRT[Btn7LXmtr2] == 1 && vexRT[Btn7UXmtr2] == 0 && vexRT[Btn7RXmtr2] == 0 && vexRT[Btn7DXmtr2] == 0){
-			ClearTimer(T1);
-			while(time1[T1] < 1000){
-				motor[LauncherLeftOutside] = -60;
-				motor[LauncherRightOutside] = -60;
-				motor[LauncherLeftInside] = -60;
-				motor[LauncherRightInside] = -60;
-			}
-			while(vexRT[Btn7LXmtr2] == 1){
-				motor[LauncherLeftOutside] = -45;
-				motor[LauncherRightOutside] = -45;
-				motor[LauncherLeftInside] = -45;
-				motor[LauncherRightInside] = -45;
-			}
-		}
-		//7U -- Medium
-		if(vexRT[Btn7LXmtr2] == 0 && vexRT[Btn7UXmtr2] == 1 && vexRT[Btn7RXmtr2] == 0 && vexRT[Btn7DXmtr2] == 0){
-			ClearTimer(T1);
-			while(time1[T1] < 1000){
-				motor[LauncherLeftOutside] = -67;
-				motor[LauncherRightOutside] = -67;
-				motor[LauncherLeftInside] = -67;
-				motor[LauncherRightInside] = -67;
-			}
-			while(vexRT[Btn7UXmtr2] == 1){
-				motor[LauncherLeftOutside] = -50;
-				motor[LauncherRightOutside] = -50;
-				motor[LauncherLeftInside] = -50;
-				motor[LauncherRightInside] = -50;
-			}
-		}
-		//7R -- High
-		if(vexRT[Btn7LXmtr2] == 0 && vexRT[Btn7UXmtr2] == 0 && vexRT[Btn7RXmtr2] == 1 && vexRT[Btn7DXmtr2] == 0){
-			ClearTimer(T1);
-			while(time1[T1] < 1000){
-				motor[LauncherLeftOutside] = -76;
-				motor[LauncherRightOutside] = -76;
-				motor[LauncherLeftInside] = -76;
-				motor[LauncherRightInside] = -76;
-			}
-			while(vexRT[Btn7RXmtr2] == 1){
-				motor[LauncherLeftOutside] = -56;
-				motor[LauncherRightOutside] = -56;
-				motor[LauncherLeftInside] = -56;
-				motor[LauncherRightInside] = -56;
-			}
-		}
-		//7D -- Field-Centric
-		if(vexRT[Btn7LXmtr2] == 0 && vexRT[Btn7UXmtr2] == 0 && vexRT[Btn7RXmtr2] == 0 && vexRT[Btn7DXmtr2] == 1 ){
-			/*motor[LauncherLeftOutside] =  -1 * launcherPower;
-			motor[LauncherRightOutside] = -1 * launcherPower;
-			motor[LauncherLeftInside] =   -1 * launcherPower;
-			motor[LauncherRightInside] =  -1 * launcherPower;
-			*/
-			ClearTimer(T1);
-			while(time1[T1] < 1000){
-				motor[LauncherLeftOutside] = -83;
-				motor[LauncherRightOutside] = -83;
-				motor[LauncherLeftInside] = -83;
-				motor[LauncherRightInside] = -83;
-			}
-			while(vexRT[Btn7DXmtr2] == 1){
-				motor[LauncherLeftOutside] = -65;
-				motor[LauncherRightOutside] = -65;
-				motor[LauncherLeftInside] = -65;
-				motor[LauncherRightInside] = -65;
-			}
-
-		}
-	}
-
-}
 
 //function that allows for the control of the turntable
 void ttable()
@@ -145,7 +58,7 @@ void drive(int frontLeft, int frontRight, int backLeft, int backRight)
 	motor[BackLeftDrive] =  backLeft;
 }
 
-//Positive values rotate the launcher right, Negative values rotate the launcher left
+//Positive values rotate the launcher left, Negative values rotate the launcher right
 void turnLauncher(int power)
 {
 	motor[Turntable] = power;
@@ -159,5 +72,131 @@ void launch(int power)
 	motor[LauncherRightOutside] = -power;
 	motor[LauncherLeftInside] =   -power;
 	motor[LauncherRightInside] =  -power;
+
+}
+
+void launchVel(int velocity){
+	ClearTimer(T1);
+	//higher starting value
+	//shorter increments - checks
+	//Set launcherpower as float
+	/*vel_difference = rightFlywheelVelocity - velocity;
+	if(vel_difference > 0){
+		if(vel_difference > 200 ){
+			launcherPower -=4;
+			while(time1[T1] < 333) launch(launcherPower);
+		}
+		else if(vel_difference > 125 && vel_difference < 200 ){
+			launcherPower -=3;
+			while(time1[T1] < 400) launch(launcherPower);
+		}
+		else if(vel_difference > 66 && vel_difference < 125 ){
+			launcherPower -=2;
+			while(time1[T1] < 450) launch(launcherPower);
+		}
+		else if(vel_difference > 25 && vel_difference < 66 ){
+			launcherPower -=1;
+			while(time1[T1] < 1000) launch(launcherPower);
+		}
+		else{
+			while(time1[T1] < 333) launch(launcherPower);
+		}
+	}
+	else if(vel_difference < 0){
+		if(vel_difference < -400 ){
+			launcherPower +=3;
+			while(time1[T1] < 333) launch(launcherPower);
+		}
+		else if(vel_difference < -200 && vel_difference > -400){
+			launcherPower +=2;
+			while(time1[T1] < 400) launch(launcherPower);
+		}
+		else if(vel_difference < -100 && vel_difference > -200){
+			launcherPower +=1;
+			while(time1[T1] < 500) launch(launcherPower);
+		}
+		else if(vel_difference < -25 && vel_difference > -100){
+			launcherPower +=1;
+			while(time1[T1] < 1000) launch(launcherPower);
+		}
+		else{
+			while(time1[T1] < 333) launch(launcherPower);
+		}
+	}
+	else launch(launcherPower);
+	*/
+
+	/*if(rightFlywheelVelocity > -800){
+		launcherPower += 1;
+	}
+	else if(rightFlywheelVelocity < -850){
+		launcherPower -=1;
+	}
+	while(time1[T1] < 333){
+		motor[LauncherLeftOutside] =  -1 * launcherPower;
+		motor[LauncherRightOutside] = -1 * launcherPower;
+		motor[LauncherLeftInside] =   -1 * launcherPower;
+		motor[LauncherRightInside] =  -1 * launcherPower;
+	}*/
+}
+
+//function that controls the power of the launcher
+//loops continuosly as the button is pressed
+task launcher()
+{
+	// Highest -------> Lowest
+	// 7U 7R 7D 7L 8U 8R 8D 8L
+	while(1==1){
+		if(vexRT[Btn8U] == 1){
+ 			launcherPower += 1;
+			wait1Msec(500);
+		}
+		else if(vexRT[Btn8D] == 1){
+			launcherPower -= 1;
+			wait1Msec(500);
+		}
+		//None pressed
+		if(vexRT[Btn7LXmtr2] == 0 && vexRT[Btn7UXmtr2] == 0 && vexRT[Btn7RXmtr2] == 0 && vexRT[Btn7DXmtr2] == 0 && vexRT[Btn8DXmtr2] == 0){
+			motor[LauncherLeftOutside] =  0;
+			motor[LauncherRightOutside] = 0;
+			motor[LauncherLeftInside] =   0;
+			motor[LauncherRightInside] =  0;
+			launcherPower = 55;
+		}
+		//7L -- Lowest
+		if(vexRT[Btn7LXmtr2] == 1 && vexRT[Btn7UXmtr2] == 0 && vexRT[Btn7RXmtr2] == 0 && vexRT[Btn7DXmtr2] == 0 && vexRT[Btn8DXmtr2] == 0){
+			//launchVel(475);
+			ClearTimer(T1);
+			while(time1[T1] < 1000) launch(55);
+			while(vexRT[Btn7LXmtr2] == 1) launch(45);
+		}
+		//7U -- Medium
+		if(vexRT[Btn7LXmtr2] == 0 && vexRT[Btn7UXmtr2] == 1 && vexRT[Btn7RXmtr2] == 0 && vexRT[Btn7DXmtr2] == 0 && vexRT[Btn8DXmtr2] == 0){
+			//launchVel(550);
+			ClearTimer(T1);
+			while(time1[T1] < 1000) launch(65);
+			while(vexRT[Btn7UXmtr2] == 1) launch(50);
+		}
+		//7R -- High
+		if(vexRT[Btn7LXmtr2] == 0 && vexRT[Btn7UXmtr2] == 0 && vexRT[Btn7RXmtr2] == 1 && vexRT[Btn7DXmtr2] == 0 && vexRT[Btn8DXmtr2] == 0){
+			//launchVel(650);
+			ClearTimer(T1);
+			while(time1[T1] < 1000) launch(75);
+			while(vexRT[Btn7RXmtr2] == 1) launch(60);
+		}
+		//7D -- Field-Centric
+		if(vexRT[Btn7LXmtr2] == 0 && vexRT[Btn7UXmtr2] == 0 && vexRT[Btn7RXmtr2] == 0 && vexRT[Btn7DXmtr2] == 1 && vexRT[Btn8DXmtr2] == 0){
+			//launchVel(800);
+			ClearTimer(T1);
+			while(time1[T1] < 1000) launch(85);
+			while(vexRT[Btn7DXmtr2] == 1) launch(70);
+		}
+		/*if(vexRT[Btn7LXmtr2] == 0 && vexRT[Btn7UXmtr2] == 0 && vexRT[Btn7RXmtr2] == 0 && vexRT[Btn7DXmtr2] == 0 && vexRT[Btn8DXmtr2] == 1){
+			motor[LauncherLeftOutside] = -1 * launcherPower;
+			motor[LauncherRightOutside] = -1 * launcherPower;
+			motor[LauncherLeftInside] = -1 * launcherPower;
+			motor[LauncherRightInside] = -1 * launcherPower;
+		}*/
+	}
 
 }
